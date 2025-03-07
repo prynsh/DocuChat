4f519664c3f4c00f1c1293a6ea9825da2c842aa3
Priyansh Verma <priyansh2112@gmail.com>
2025-03-07 15:44:30 +0530

    Added Drizzle and PG

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,16 @@
-'use client'
+"use client";
 
+import FileUpload from "@/components/FileUpload";
 
 export default function Dashboard() {
-
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-2xl font-bold">Welcome to the Dashboard!</h1>
+    <div className="bg-gradient-to-r from-[#dfe2fe] via-[#b1cbfa] to-[#8e98f5] h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-3xl font-semibold text-gray-800 mb-6">
+          Chat with Any PDF
+        </h1>
+        <FileUpload />
+      </div>
     </div>
-  )
+  );
 }
--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -10,7 +10,7 @@ import { FileText } from "lucide-react"
 
 export default function TopBar() {
   return (
-    <header className="sticky top-0 z-50 flex justify-between items-center px-6 py-4 bg-white/80 backdrop-blur-sm shadow-sm">
+    <header className="sticky top-0 z-50 flex justify-between items-center px-6 py-4 bg-white/80 backdrop-blur-xl shadow-sm">
       <Link href="/" className="flex items-center space-x-2">
         <FileText className="h-6 w-6 text-primary" />
         <div className="text-xl font-bold cursor-pointer">DocuChat</div>
